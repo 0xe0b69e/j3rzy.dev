5e6869b44848a0a7c0e3e0058184b42dad815872
0xe0b69e <github@j3rzy.dev>
2024-07-11 17:49:22 +0200

    Added the base

---
+++ lib/font.ts
@@ -0,0 +1,4 @@
+import { Inter, Varela_Round } from "next/font/google";
+import { NextFont } from "next/dist/compiled/@next/font";
+
+export const varelaRound: NextFont = Varela_Round({ subsets: [ "latin" ], weight: "400" });
