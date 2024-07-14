a42bc1237974c9c47bdea82ee4820a06ac7821ad
0xe0b69e <github@j3rzy.dev>
2024-07-15 00:04:01 +0200

    Lowered shadow

--- components/files/Navbar.tsx
+++ components/files/Navbar.tsx
@@ -7,7 +7,7 @@ export default function Navbar (): JSX.Element
 {
   return (
     <nav className={cn(
-      "w-full h-16 top-0 bg-surface dark:bg-surface-dark transition-all duration-300 shadow-lg fixed z-50 items-center flex justify-end p-2",
+      "w-full h-16 top-0 bg-surface dark:bg-surface-dark transition-all duration-300 shadow fixed z-50 items-center flex justify-end p-2",
     )}>
       <SignIn />
     </nav>
