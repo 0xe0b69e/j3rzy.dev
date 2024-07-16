97c413dcca8c58575f5e669d2d911dbef9439c97
0xe0b69e <github@j3rzy.dev>
2024-07-16 17:39:41 +0200

    Changed the avatar url

--- lib/fallbackData.ts
+++ lib/fallbackData.ts
@@ -1,7 +1,7 @@
 import { Full, UserData } from "@/lib/definitions";
 
 export const fallbackDiscordData: Full<UserData> = {
-  avatar: `https://cdn.discordapp.com/avatars/719890634294427669/265284f97d52d0d50242290b83781fa0.webp?size=4096`,
+  avatar: `https://cdn.discordapp.com/avatars/719890634294427669/ceb05c6170da1cfce64c608926379acc.webp?size=4096`,
   username: "0xe0b69e",
   global_name: "Jerzy"
 }
