50633416047c09e0f9febd4210faa523e5e88b99
0xe0b69e <github@j3rzy.dev>
2024-07-16 17:13:09 +0200

    Added back button

--- app/(files)/file/[id]/page.tsx
+++ app/(files)/file/[id]/page.tsx
@@ -7,6 +7,7 @@ import { formatBytes, formatDate } from "@/lib/utils";
 import Link from "next/link";
 import { auth } from "@/auth";
 import { Session } from "next-auth";
+import { FaArrowLeft } from "react-icons/fa6";
 
 interface PageProps
 {
@@ -59,34 +60,38 @@ export default async function Page ({ params, searchParams }: PageProps): Promis
   const uploader: Prisma.User | null = file ? await getUserById(file.id) : null;
   
   return (
-    <main className="pt-16 bg-background dark:bg-background-dark flex w-full h-full items-center justify-center">
-      {(!!file && (!file.isPrivate || session?.user.id === file.userId))
-        ? (
-          <div
-            className="p-8 bg-background-light dark:bg-background-dark rounded-lg shadow-lg flex flex-col items-center pb-4">
-            <h1
-              className="text-3xl font-mono bg-background dark:bg-background-dark shadow-inner border-4 border-background dark:border-background-dark py-2 px-6"
-              style={{ boxShadow: "inset 0 4px 8px rgba(0, 0, 0, 0.6)" }}
-            >{file.name}</h1>
-            <p>Uploaded by {uploader?.name ?? "Anonymous"}</p>
-            <p>Size: {formatBytes(file.size)}</p>
-            <p className="mb-3">Uploaded at: {formatDate(file.createdAt)}</p>
-            <Link
-              href={`/api/file?id=${file.id}`}
-              target="_blank"
-              className="py-1 px-8 bg-gradient-to-b from-secondary to-primary hover:from-secondary-dark hover:to-primary-dark rounded-lg text-center text-white"
-            >
-              Download
-            </Link>
-          </div>
-        )
-        : (
-          <div className="p-8 bg-background-light dark:bg-background-dark rounded-lg shadow-lg flex items-center justify-center text-center flex-col">
-            <h1 className="text-3xl font-bold">File not found</h1>
-            <p>The file you&#39;re looking for doesn&#39;t exist.</p>
-          </div>
-        )
-      }
-    </main>
+    <>
+      <Link href="/files" className="fixed top-16 left-0 p-2" title="Go back"><FaArrowLeft className="w-10 h-10" /></Link>
+      <main className="pt-16 bg-background dark:bg-background-dark flex w-full h-full items-center justify-center">
+        {(!!file && (!file.isPrivate || session?.user.id === file.userId))
+          ? (
+            <div
+              className="p-8 bg-background-light dark:bg-background-dark rounded-lg shadow-lg flex flex-col items-center pb-4">
+              <h1
+                className="text-3xl font-mono bg-background dark:bg-background-dark shadow-inner border-4 border-background dark:border-background-dark py-2 px-6 text-wrap"
+                style={{ boxShadow: "inset 0 4px 8px rgba(0, 0, 0, 0.6)" }}
+              >{file.name}</h1>
+              <p className="text-wrap">Uploaded by {uploader?.name ?? "Anonymous"}</p>
+              <p className="text-wrap">Size: {formatBytes(file.size)}</p>
+              <p className="mb-3 text-wrap">Uploaded at: {formatDate(file.createdAt)}</p>
+              <Link
+                href={`/api/file?id=${file.id}`}
+                target="_blank"
+                className="py-1 px-8 bg-gradient-to-b from-secondary to-primary hover:from-secondary-dark hover:to-primary-dark rounded-lg text-center text-white"
+              >
+                Download
+              </Link>
+            </div>
+          )
+          : (
+            <div
+              className="p-8 bg-background-light dark:bg-background-dark rounded-lg shadow-lg flex items-center justify-center text-center flex-col">
+              <h1 className="text-3xl font-bold text-wrap">File not found</h1>
+              <p className="text-wrap">The file you&#39;re looking for doesn&#39;t exist.</p>
+            </div>
+          )
+        }
+      </main>
+    </>
   );
 }
