cf945cad95aca14baad93b310147d86777bb0347
0xe0b69e <github@j3rzy.dev>
2024-07-17 00:53:27 +0200

    Added a cookie

--- auth.config.ts
+++ auth.config.ts
@@ -2,5 +2,16 @@ import { NextAuthConfig } from "next-auth";
 import Google from "@auth/core/providers/google";
 
 export default {
-  providers: [Google]
+  providers: [ Google ],
+  cookies: {
+    pkceCodeVerifier: {
+      name: "next-auth.pkce.code_verifier",
+      options: {
+        httpOnly: true,
+        sameSite: "none",
+        path: "/",
+        secure: process.env.NODE_ENV === "production",
+      },
+    }
+  }
 } satisfies NextAuthConfig;
