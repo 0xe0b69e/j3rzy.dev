bdfdeddc73b484ff4102c70f88b86809f9ad4a6e
0xe0b69e <github@j3rzy.dev>
2024-07-31 18:23:39 +0200

    Added simple main page

--- app/page.tsx
+++ app/page.tsx
@@ -1,6 +1,53 @@
-export default function Home (): JSX.Element
-{
+"use client";
+
+import { useEffect, useRef } from "react";
+
+export default function Home(): JSX.Element {
+  const text: string = "I'm in your walls...";
+  const letterRefs = useRef<(HTMLSpanElement | null)[]>([]);
+  
+  function getRandomInt(min: number, max: number) {
+    return Math.random() * (max - min) + min;
+  }
+  
+  useEffect(() => {
+    function moveRandomly(element: HTMLElement) {
+      const x: number = getRandomInt(-1, 1);
+      const y: number = getRandomInt(-1, 1);
+      const speed: number = getRandomInt(0.05, 0.2);
+      
+      element.style.transition = `transform ${speed}s linear`;
+      element.style.transform = `translate(${x}px, ${y}px)`;
+    }
+    
+    const intervals = letterRefs.current.map((element, index) => {
+      if (!element) return null;
+      
+      const intervalId: NodeJS.Timeout = setInterval(() => moveRandomly(element), 100);
+      
+      moveRandomly(element);
+      
+      return intervalId;
+    });
+    
+    return () => intervals.forEach(intervalId => intervalId && clearInterval(intervalId));
+  }, []);
+  
   return (
-    <></>
-  )
+    <main className="w-full h-full bg-black text-white flex items-center justify-center font-mono text-3xl text-center">
+      <div>
+        {text.split("").map((char, index) => (
+          <span
+            key={index}
+            ref={(el) => {
+              letterRefs.current[index] = el;
+            }}
+            className="inline-block"
+          >
+            {char === " " ? "\u00A0" : char}
+          </span>
+        ))}
+      </div>
+    </main>
+  );
 }
--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,6 +1,5 @@
 import type { Config } from "tailwindcss";
 import defaultTheme from "tailwindcss/defaultTheme";
-import plugin from "tailwindcss/plugin";
 
 const config: Config = {
     content: [
@@ -12,17 +11,6 @@ const config: Config = {
       extend: {
         screens: {
           "sm": "690px",
-        },
-        keyframes: {
-          randomMove: {
-            '0%, 100%': { transform: 'translate(0, 0)' },
-            '25%': { transform: 'translate(-2px, 2px)' },
-            '50%': { transform: 'translate(2px, -2px)' },
-            '75%': { transform: 'translate(-2px, -2px)' },
-          },
-        },
-        animation: {
-          randomMove: 'randomMove 1s infinite',
         },
       },
       screens: {
