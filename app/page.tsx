1871ec7d8bb766c8219c600f558edf001195462c
0xe0b69e <github@j3rzy.dev>
2024-09-06 10:14:28 +0200

    Feed the woods

--- app/page.tsx
+++ app/page.tsx
@@ -1,53 +1,19 @@
 "use client";
 
-import { useEffect, useRef } from "react";
+import { cn } from "@/lib/utils";
+import { NerkoOne } from "@/lib/fonts";
 
 export default function Home(): JSX.Element {
-  const text: string = "I'm in your walls...";
-  const letterRefs = useRef<(HTMLSpanElement | null)[]>([]);
-  
-  function getRandomInt(min: number, max: number) {
-    return Math.random() * (max - min) + min;
-  }
-  
-  useEffect(() => {
-    function moveRandomly(element: HTMLElement) {
-      const x: number = getRandomInt(-1, 1);
-      const y: number = getRandomInt(-1, 1);
-      const speed: number = getRandomInt(0.05, 0.2);
-      
-      element.style.transition = `transform ${speed}s linear`;
-      element.style.transform = `translate(${x}px, ${y}px)`;
-    }
-    
-    const intervals = letterRefs.current.map((element, index) => {
-      if (!element) return null;
-      
-      const intervalId: NodeJS.Timeout = setInterval(() => moveRandomly(element), 100);
-      
-      moveRandomly(element);
-      
-      return intervalId;
-    });
-    
-    return () => intervals.forEach(intervalId => intervalId && clearInterval(intervalId));
-  }, []);
-  
   return (
-    <main className="w-full h-full bg-black text-white flex items-center justify-center font-mono text-3xl text-center">
-      <div>
-        {text.split("").map((char, index) => (
-          <span
-            key={index}
-            ref={(el) => {
-              letterRefs.current[index] = el;
-            }}
-            className="inline-block"
-          >
-            {char === " " ? "\u00A0" : char}
-          </span>
-        ))}
-      </div>
+    <main className="w-full h-full bg-[#3a1911] flex sm:p-20 flex-col items-center">
+      <h1
+        className={cn("uppercase text-[100px] text-[#a97e4f] m-0", NerkoOne.className)}
+        style={{ textShadow: "-7px 7px 2px #000" }}
+      >Feed the woods</h1>
+      <p
+        className="font-serif text-[30px] font-semibold"
+        style={{ textShadow: "-2px 2px 1px #000" }}
+      >Go into the woods.</p>
     </main>
   );
 }
---
+++ lib/fonts.ts
@@ -0,0 +1,4 @@
+import { NextFont } from "next/dist/compiled/@next/font";
+import { Nerko_One } from "next/font/google";
+
+export const NerkoOne: NextFont = Nerko_One({ subsets: [ "latin" ], weight: "400" });
